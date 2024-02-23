0e407931e30a80f43c50c1f2fb46e1c4e7144798
noorimanr@gmail.com <noorimanr@gmail.com>
2024-02-24 01:25:53 +0800

    first commit

---
+++ components/about.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+
+export default function About() {
+  return (
+    <motion.section
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
+      <SectionHeading>ABOUT ME</SectionHeading>
+      <p className="mb-3">
+        After graduating with a degree in{" "}
+        <span className="font-medium">Mathematics</span>, I decided to pursue my
+        passion for programming. I enrolled in a coding bootcamp and learned{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally cracking a solution. I primarily use{" "}
+        <span className="font-medium">
+          React, Next.js, Node.js, Go, Python, Typescript, and MongoDB
+        </span>{" "}
+        for my development projects, and I'm always eager to learn new
+        technologies. Currently, I'm looking for a{" "}
+        <span className="font-medium">full-time position</span> as a software
+        developer.
+      </p>
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        online games, watching anime or movies, and reading. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history, politics, and philosophy</span>.
+      </p>
+    </motion.section>
+  );
+}
