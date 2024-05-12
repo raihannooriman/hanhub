668ea5f757c0ac4535ce3a01dc8bb5621f036906
noorimanr@gmail.com <noorimanr@gmail.com>
2024-05-12 21:33:22 +0800

    bersih-bersih

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,12 +6,8 @@ export default function Footer() {
       className="mb-10 px-4 text-center text-gray-500
     "
     >
-      <small className="mb-2 block text-xs">
+      <p className="mb-2 block text-xs">
         &copy; 2024 Raihan. All right reserved.
-      </small>
-      <p className="text-xs">
-        <span className="font-semibold">This website</span> build with React,
-        Next.js, Typescript, and Tailwind CSS.
       </p>
     </footer>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -64,20 +64,6 @@ export const projectsData = [
     tags: ["React", "Next.js", "MongoDB", "Tailwind"],
     imageUrl: corpcommentImg,
   },
-  {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "mySQL", "Tailwind"],
-    imageUrl: wordanalyticsImg,
-  },
 ] as const;
 
 export const skillsData = [
